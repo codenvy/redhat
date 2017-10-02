d27395ee6cd403fe5a3f57a41372bafd008d89c4
Anatoliy Bazko <abazko@redhat.com>
2017-10-02 17:54:08 +0300

    Fix up

--- site/app/site/branding/header-script.js
+++ site/app/site/branding/header-script.js
@@ -2,7 +2,7 @@
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
+    var results = regex.exec(window.location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
